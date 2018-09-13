6e2c520a7a9f18b772d680c711cbc12754afb1c0
Robert Ancell <robert.ancell@canonical.com>
2018-09-13 18:11:12 +1200

    Use a Gtk template

--- mp-window.c
+++ mp-window.c
@@ -10,11 +10,15 @@ G_DEFINE_TYPE (MpWindow, mp_window, GTK_TYPE_WINDOW)
 void
 mp_window_class_init (MpWindowClass *klass)
 {
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+    gtk_widget_class_set_template_from_resource (widget_class, "/com/ubuntu/multipass/mp-window.ui");
 }
 
 void
 mp_window_init (MpWindow *window)
 {
+    gtk_widget_init_template (GTK_WIDGET (window));
 }
 
 MpWindow *
