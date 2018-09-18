6125d0b6f56f8f7e1210ebdf15c7508f8a70f5c2
Robert Ancell <robert.ancell@canonical.com>
2018-09-18 14:28:15 +0200

    Add some copyright headers

--- mp-window.c
+++ mp-window.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2018 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ * license.
+ */
+
 #include <vte/vte.h>
 
 #include "mp-client.h"
